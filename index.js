0ddcfcb6ca9508f9dbfa78b41b25ab8918bc04c1
Kakoroussos <kakoroussos@gmail.com>
2025-06-09 21:28:39 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{ padding: '2rem', textAlign: 'center' }}>
+      <h1>Bienvenido a Rentering 🛎️</h1>
+      <p>Encuentra tu lugar. Ofrece tu hospedaje.</p>
+    </div>
+  );
+}
