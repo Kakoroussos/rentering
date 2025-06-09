9e1d68a890e2047c07c4359a75bdf453f6e88522
Kakoroussos <kakoroussos@gmail.com>
2025-06-09 21:32:34 +0200

    Delete dashboard.js

--- dashboard.js
+++
@@ -1,8 +0,0 @@
-export default function Dashboard() {
-  return (
-    <div style={{ padding: '2rem', textAlign: 'center' }}>
-      <h2>Panel de usuario</h2>
-      <p>Bienvenido al sistema Rentering</p>
-    </div>
-  );
-}
